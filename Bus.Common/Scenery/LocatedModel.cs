bcc732e872ff2a7e34617e8dadc342ff6e1e7faa
automatic9045 <67314487+automatic9045@users.noreply.github.com>
2025-10-16 00:41:31 +0900

    feat: 剛体表現用のDynamicLocatedModelクラスを追加

--- Bus.Common/Scenery/LocatedModel.cs
+++ Bus.Common/Scenery/LocatedModel.cs
@@ -6,7 +6,6 @@ using System.Text;
 using System.Threading.Tasks;
 
 using BepuPhysics;
-using Vortice.Direct3D11;
 
 using Bus.Common.Physics;
 using Bus.Common.Rendering;
@@ -17,13 +16,20 @@ namespace Bus.Common.Scenery
     {
         public IModel Model { get; }
         public Matrix4x4 InitialLocator { get; }
-        public Matrix4x4 Locator { get; set; }
+        public Matrix4x4 InitialLocatorInverse { get; }
+        public virtual Matrix4x4 Locator { get; set; } = Matrix4x4.Identity;
 
-        public LocatedModel(IModel model, Matrix4x4 locator)
+        protected LocatedModel(IModel model, Matrix4x4 locator, bool setLocator)
         {
             Model = model;
             InitialLocator = locator;
-            Locator = locator;
+            Matrix4x4.Invert(locator, out Matrix4x4 initialLocatorInverse);
+            InitialLocatorInverse = initialLocatorInverse;
+            if (setLocator) Locator = locator;
+        }
+
+        public LocatedModel(IModel model, Matrix4x4 locator) : this(model, locator, true)
+        {
         }
 
         public static LocatedModel CreateStaticOrNonCollision(Simulation simulation, IModel model, Matrix4x4 locator)
@@ -37,6 +43,15 @@ namespace Bus.Common.Scenery
             StaticDescription desc = new StaticDescription((locator * model.Collider.Transform).ToRigidPose(), model.Collider.ShapeIndex);
             StaticHandle handle = simulation.Statics.Add(desc);
             return new StaticLocatedModel(simulation, model, handle, locator);
+        }
+
+        public static DynamicLocatedModel CreateDynamic(Simulation simulation, ICollidableModel model, float mass, Matrix4x4 locator)
+        {
+            BodyInertia inertia = model.Collider.ComputeInertia(mass);
+            BodyDescription desc = BodyDescription.CreateDynamic(
+                (locator * model.Collider.Transform).ToRigidPose(), inertia, model.Collider.ShapeIndex, 0.001f);
+            BodyHandle handle = simulation.Bodies.Add(desc);
+            return new DynamicLocatedModel(simulation, model, handle, locator);
         }
 
         public void Draw(DrawContext context)
@@ -72,6 +87,44 @@ namespace Bus.Common.Scenery
         {
             Matrix4x4 locator = Locator * plateOffset.Transform;
             Simulation.Statics[Handle].Pose = (locator * Model.Collider.Transform).ToRigidPose();
+        }
+    }
+
+
+    public class DynamicLocatedModel : LocatedModel
+    {
+        private readonly Simulation Simulation;
+
+        public new ICollidableModel Model { get; }
+        public BodyHandle Handle { get; }
+        public BodyReference Body => Simulation.Bodies[Handle];
+
+        public override Matrix4x4 Locator
+        {
+            get => base.Locator;
+            set
+            {
+                base.Locator = value;
+                BodyLocator = Locator;
+            }
+        }
+
+        protected Matrix4x4 BodyLocator
+        {
+            get => Model.Collider.TransformInverse * Body.Pose.ToMatrix4x4();
+            set => Body.Pose = (Model.Collider.Transform * value).ToRigidPose();
+        }
+
+        internal protected DynamicLocatedModel(Simulation simulation, ICollidableModel model, BodyHandle handle, Matrix4x4 locator) : base(model, locator, false)
+        {
+            Simulation = simulation;
+            Model = model;
+            Handle = handle;
+        }
+
+        public void SyncLocator()
+        {
+            base.Locator = BodyLocator;
         }
     }
 }
