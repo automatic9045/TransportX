9d40455912ce532c1eb59f3b393776bb389b6e87
automatic9045 <67314487+automatic9045@users.noreply.github.com>
2026-01-04 20:26:18 +0900

    refactor: Lane、LaneLayoutの対をキャッシュするように

--- Bus.Common/Scenery/Networks/Lane.cs
+++ Bus.Common/Scenery/Networks/Lane.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
@@ -7,36 +8,47 @@ using System.Threading.Tasks;
 
 namespace Bus.Common.Scenery.Networks
 {
-    public sealed class Lane
+    public class Lane
     {
+        private static readonly ConcurrentDictionary<Lane, Lane> Oppositions = [];
+
+
         public LaneKind Kind { get; }
         public FlowDirections Directions { get; }
         public Vector2 Position { get; }
+
+        public Lane Opposition { get; }
+
+        private Lane(LaneKind kind, FlowDirections directions, Vector2 position, Lane opposition)
+        {
+            Kind = kind;
+            Directions = directions;
+            Position = position;
+
+            Opposition = opposition;
+            Oppositions.TryAdd(this, opposition);
+        }
 
         public Lane(LaneKind kind, FlowDirections directions, Vector2 position)
         {
             Kind = kind;
             Directions = directions;
             Position = position;
+
+            Opposition = Oppositions.GetOrAdd(this, x =>
+            {
+                Lane opposition = new Lane(Kind, Directions.GetOpposition(), new Vector2(-Position.X, Position.Y), this);
+                return opposition;
+            });
         }
 
         public override string ToString() => $"{Position}: {Kind.Name}, {Directions}";
 
-        public bool IsOpposite(Lane other)
+        public bool IsOppositeOf(Lane other)
         {
             return Kind == other.Kind
                 && Directions.IsOppositeOf(other.Directions)
                 && Position.X == -other.Position.X && Position.Y == other.Position.Y;
-        }
-
-        public Lane CreateCopy()
-        {
-            return new Lane(Kind, Directions, Position);
-        }
-
-        public Lane CreateOpposite()
-        {
-            return new Lane(Kind, Directions.GetOpposition(), new Vector2(-Position.X, Position.Y));
         }
     }
 }
--- Bus.Common/Scenery/Networks/LaneLayout.cs
+++ Bus.Common/Scenery/Networks/LaneLayout.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
@@ -9,11 +10,35 @@ namespace Bus.Common.Scenery.Networks
 {
     public class LaneLayout
     {
+        private static readonly ConcurrentDictionary<LaneLayout, LaneLayout> Oppositions = [];
+
+
         public IReadOnlyList<Lane> Lanes { get; }
+
+        public LaneLayout Opposition { get; }
+
+        private LaneLayout(IReadOnlyList<Lane> lanes, LaneLayout opposition)
+        {
+            Lanes = lanes;
+
+            Opposition = opposition;
+            Oppositions.TryAdd(this, opposition);
+        }
 
         public LaneLayout(IReadOnlyList<Lane> lanes)
         {
             Lanes = lanes;
+
+            Opposition = Oppositions.GetOrAdd(this, x =>
+            {
+                Lane[] oppositeLanes = Lanes
+                    .Reverse()
+                    .Select(lane => lane.Opposition)
+                    .ToArray();
+
+                LaneLayout opposition = new LaneLayout(oppositeLanes, this);
+                return opposition;
+            });
         }
 
         public LaneLayout(params Lane[] lanes) : this((IReadOnlyList<Lane>)lanes)
@@ -29,29 +54,10 @@ namespace Bus.Common.Scenery.Networks
                 Lane from = Lanes[i];
                 Lane to = other.Lanes[Lanes.Count - 1 - i];
 
-                if (!from.IsOpposite(to)) return false;
+                if (!from.IsOppositeOf(to)) return false;
             }
 
             return true;
-        }
-
-        public LaneLayout CreateCopy()
-        {
-            Lane[] newLanes = Lanes
-                .Select(lane => lane.CreateCopy())
-                .ToArray();
-
-            return new LaneLayout(newLanes);
-        }
-
-        public LaneLayout CreateOpposition()
-        {
-            Lane[] newLanes = Lanes
-                .Reverse()
-                .Select(lane => lane.CreateOpposite())
-                .ToArray();
-
-            return new LaneLayout(newLanes);
         }
 
         public LanePin[] CreatePins(NetworkPort port)
